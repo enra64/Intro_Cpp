605b7110bc37db09c92a84a2d7a5aee6e1ca9663
enra64 <sujomear@arcor.de>
2016-05-20 15:59:53 +0200

    removed bad task 5 impl

--- Task5/object.cpp
+++ Task5/object.cpp
@@ -1,85 +1,31 @@
 #ifdef _MSC_VER
 #define _CRT_SECURE_NO_WARNINGS
 #endif
-
-
 #include "object.h"
-#include <string>
+#include <iostream>
+#include <string.h>
 
-
-
-template <typename BERT>
-Object<BERT>::Object(const Object<BERT> &o) :
-        m_name(o.m_name) {
-    std::cout << "Copied object with name: " << m_name << std::endl;
-}
-
-template <typename BERT>
-Object<BERT>::~Object() {
-    std::cout << "Destroyed object with name: " << m_name << std::endl;
-}
-template <typename BERT>
-Object<BERT> &Object<BERT>::operator+=(Object<BERT> &o) {
-    mData += o.getData();
-    return *this;
-}
-template <typename BERT>
-Object<BERT> &Object<BERT>::operator-=(Object<BERT> &o) {
-    mData -= o.getData();
-    return *this;
-}
-template <typename BERT>
-Object<BERT> &Object<BERT>::operator*=(Object<BERT> &o) {
-    mData *= o.getData();
-    return *this;
-}
-template <typename BERT>
-Object<BERT> &Object<BERT>::operator/=(Object<BERT> &o) {
-    mData /= o.getData();
-    return *this;
-}
-template <typename BERT>
-Object<BERT> &Object<BERT>::operator%=(Object<BERT> &o) {
-    mData %= o.getData();
-    return *this;
-}
-
-template <typename BERT>
-Object<BERT>::Object(const char *name, const BERT data) {
+Object::Object(const char* name) {
     m_name = new char[strlen(name)];
-    mData = data;
     strcpy(m_name, name);
     std::cout << "Created object with name: " << m_name << std::endl;
 }
 
-template <typename BERT>
-std::ostream &operator<<(std::ostream &stream, const Object<BERT> &o) {
-    stream << "|-|" << o.GetName() << ":" << o.getData() << "|-|";
-    return stream;
-}
-template <typename BERT>
-std::istream &operator>>(std::istream &lhs, Object<BERT> &rhs) {
-    std::string input;
-    lhs >> input;
-    const char* name = input.substr(0, input.find(":")).c_str();
-    char nonConstName[input.find(":") + 1];
-    memcpy(nonConstName, name, input.find(":"));
-    rhs.setName(nonConstName);
-    BERT data = input.substr(input.find(":"), std::string::npos);
-    rhs.setData(data);
-    delete name;
-    return lhs;
-}
-template <typename BERT>
-bool operator<(Object<BERT> const &lhs, Object<BERT> const &rhs) {
-    return lhs.GetName() < rhs.GetName();
-}
-template <typename BERT>
-bool operator>(Object<BERT> const &lhs, Object<BERT> const &rhs) {
-    return lhs.GetName() > rhs.GetName();
+Object::Object(const Object& o) :
+    m_name(o.m_name) {
+    std::cout << "Copied object with name: " << m_name << std::endl;
 }
 
-template class Object<int>;
-// when you implement templated whatever in the source instead of the header file, you have to force instantiation using
-// this sh*t which kinda makes me question splitting my programs to header and source
-template std::ostream &operator<<(std::ostream &stream, const Object<int> &o);
+Object::~Object() {
+    std::cout << "Destroyed object with name: " << m_name << std::endl;
+}
+
+std::ostream& operator<< (std::ostream& stream, const Object& o) {
+    stream << "|-|" << o.getName() << "|-|";
+    return stream;
+}
+
+std::ostream& operator>> (std::ostream& stream, const Object& o) {
+    stream >> o.m_name;
+    return stream;
+}
