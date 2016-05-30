8780f8bad1eea1816fc487a3cd9b96a6939f6c3a
enra64 <sujomear@arcor.de>
2016-05-30 14:26:19 +0200

    init t8

---
+++ Task8/CMakeFiles/Task8.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task8.dir/program.cpp.o"
+  "Task8.pdb"
+  "Task8"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Task8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
